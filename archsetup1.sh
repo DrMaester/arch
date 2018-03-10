6388083c789e909c619fd4567d81e93d7670c384
DrMaester <siemensdaniel98@gmail.com>
2018-03-10 23:11:24 +0100

    New Files

---
+++ archsetup1.sh
@@ -0,0 +1,52 @@
+# Installation part One
+
+# load keyboard layout
+loadkeys de
+
+# activate timecontrol over network
+timedatectl set-ntp true
+
+# format disk with fdisk
+(
+    echo o # Create a new empty DOS partition table
+    echo n # Add a new partition
+    echo p # Primary partition
+    echo 1 # Partition number
+    echo   # First sector (Accept default: 1)
+    echo   # Last sector (Accept default: varies)
+    echo a # Add a bootable flag to partition
+    echo w # Write changes
+) | sudo fdisk /dev/sda
+
+# build ext4 filesystem on it
+mkfs.ext4 /dev/sda1
+
+# mount the new partition
+mount /dev/sda1 /mnt
+
+# Move Germany Mirror to Top
+# ctrl+k (cut line) -> ctrl+u (insert line on top)
+# ctrl+x -> y -> Enter
+nano /etc/pacman.d/mirrorlist
+
+# Mirrorlist edit:
+# or move generated germany list
+mv mirrorlist.txt /etc/pacman.d/mirrorlist
+
+# install base system
+pacstrap /mnt base base-devel intel-ucode vim
+
+# generate file system table (fstab)
+genfstab -p /mnt > /mnt/etc/fstab
+
+# change root to /mnt
+arch-chroot /mnt
+
+# install and configure GRUB
+pacman -S grub os-prober
+grub-install --recheck /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# remove bootable media, restart PC
+exit
+reboot
---
+++ archsetup2.sh
@@ -0,0 +1,39 @@
+# Installation part Two
+
+# set hostname
+echo Lancelot > /etc/hostname
+
+# language settings
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+echo LC_MESSAGES=en_US.UTF-8 >> /etc/locale.conf
+
+# remove # before your language
+vim /etc/locale.gen
+locale-gen
+
+# keyboard layout
+echo KEYMAP=de_latin1 > /etc/vconsole.conf
+
+# timezone settings
+ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+
+# hosts
+echo "<ip-address>  <hostname.domain.org>   <hostname>
+127.0.0.1   localhost.localdomain   localhost
+::1     localhost.localdomain   localhost" >> /etc/hosts
+
+# edit pacman
+vim /etc/pacman.conf
+  # include multilib for wine later
+
+# sync databases
+pacman -Sy
+
+# Possible to have encrypted root filesystem
+# Create an initial ramdisk environment
+mkinitcpio -p linux
+
+# change root password
+passwd
+
+pacman -S grub bash-completion dialog wpa_supplicant network-manager nmtui zsh
